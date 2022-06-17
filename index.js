6466855f84d80ce60dbf806f4ffede0fb569585f
Lawyn <iustinftw@gmail.com>
2022-06-17 04:24:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const app = require('./server/server.js');
-require('./server/database/database');
 
 app.listen(app.get('port'), () => {
     console.log(`Server listening on port ${app.get('port')}`);
